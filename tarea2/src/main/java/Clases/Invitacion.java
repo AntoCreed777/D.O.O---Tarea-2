96beb0001984f1e09f427401d71a61c457116de0
AntoCreed777 <abenavides@desem.cl>
2024-05-02 20:57:38 -0400

    A Invitacion se le agrego el Constructor y Getters para sus atributos

--- tarea2/src/main/java/Clases/Invitacion.java
+++ tarea2/src/main/java/Clases/Invitacion.java
@@ -4,5 +4,18 @@ import java.time.Instant;
 
 public class Invitacion {
     private Instant hora;
-    public Invitacion(){};
+    private Invitable invitado;
+
+    public Invitacion(Invitable invitado){
+        this.invitado = invitado;
+        this.hora = Instant.now();
+    }
+
+    public Invitable getInvitado(){
+        return invitado;
+    }
+
+    public Instant getHora(){
+        return hora;
+    }
 }
